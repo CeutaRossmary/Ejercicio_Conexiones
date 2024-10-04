28f654cc783a3dcf566aeaeabe014504c00b4746
CeutaRossmary <77645395+CeutaRossmary@users.noreply.github.com>
2024-10-03 23:26:10 -0300

    api weather

--- Ejercicio_Conexiones/index.js
+++ Ejercicio_Conexiones/index.js
@@ -1,7 +1,7 @@
 const contenedor = document.getElementById("mostrar");
 const array = [];
 
-async function consulta() {
+async function consulta_holidays() {
     try {
         let respuesta = await fetch("https://api.boostr.cl/holidays");
         let data = await respuesta.json(); 
@@ -9,10 +9,11 @@ async function consulta() {
         
         console.log(data); 
         
-        let pintar = "<h4>Los Feriados son</h4>";
-        pintar += "<table class='table table-hover w-75'>";
-        pintar += "<thead><tr><th>Fecha</th><th>Título</th><th>Tipo</th><th>Inalienable</th><th>Extra</th></tr></thead>";
-        pintar += "<tbody class='align-baseline fw-medium'>";
+        let pintar = "<h4 class='p-5'>Los Feriados son</h4>";
+        pintar += "<table class='table table-hover  w-50 mx-auto'>";
+        pintar +=
+          "<thead class='table-dark'><tr><th>Fecha</th><th>Título</th><th>Tipo</th><th>Inalienable</th><th>Extra</th></tr></thead>";
+        pintar += "<tbody class='align-center fw-medium'>";
 
         pintar += holidays.map(holiday => {
             array.push(holiday);
@@ -36,74 +37,71 @@ async function consulta() {
 
 
 
-  async function agregar() {
+async function consulta_weather() {
     try {
-      let newUser = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: "Nuevo registro",
-          body: "Este es el nuevo registro",
-          userId: 1,
-        }),
-      });
-      let data = await newUser.json();
-      console.log(data);
+        let respuesta = await fetch("https://api.boostr.cl/weather");
+        let data = await respuesta.json(); 
+        let weathers = data.data; 
+        
+        console.log(data); 
+        
+        let pintar =
+          "<h4 class='p-5'>Estado del tiempo pricipales Ciudades de Chile</h4>";
+        pintar += "<table class='table table-hover w-50 mx-auto'>";
+        pintar +=
+          "<thead class='table-dark'><tr><th>Codigo</th><th>Ciudad</th><th>Condición</th><th>Temperatura</th></tr></thead>";
+        pintar += "<tbody class='align-center fw-medium'>";
+
+        pintar += weathers.map(weather => {
+            array.push(weather);
+            return `
+                <tr>
+                    <td>${weather.code}</td>
+                    <td>${weather.city}</td> 
+                    <td>${weather.condition}</td> 
+                    <td>${weather.temperature}ºC</td> 
+            `;
+        }).join(''); 
+
+        pintar += "</tbody></table>";  
+        contenedor.innerHTML = pintar;  
     } catch (error) {
-      console.error("Error:", error);
+        console.error("Error:", error);
     }
-  }
-  
-  async function actualizar() {
-      try{
-    let actualizar= await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id: 1,
-        title: "Titulo actualizado",
-        body: "Este es el contenido actualizado",
-        userId: 1,
-      }),
-    })
-    let dataActualizada = await actualizar.json();
-    console.log(dataActualizada);
-  }catch(error){
-      console.error("Error:", error);
-    }
-  }
-  
-  async function eliminar() {
-    try {
-      let respuesta = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-        method: "DELETE",
-      });
-   console.log(respuesta);
-      if (respuesta.ok) {
-        console.log("Registro eliminado", respuesta);
-      } else {
-        console.error("Error al eliminar el registro");
-      }
+}
+
+
+
+async function consulta_banks() {
+  try {
+    let respuesta = await fetch("https://api.boostr.cl/banks");
+    let data = await respuesta.json();
+    let banks = data.data; 
+
+    console.log(data);
+
+    let pintar = "<h4 class=' p-5'>Lista de Bancos</h4>";
+   pintar += "<table class='table table-hover w-50 mx-auto d-block'>";
+    pintar +=
+      "<thead class='table-dark'><tr><th>Codigo</th><th>Nombre</th><th>Swift</th><th>Website</th></tr></thead>";
+    pintar += "<tbody class='align-center fw-medium'>";
+
+    pintar += banks
+      .map((bank) => {
+        return `
+                <tr>
+                    <td>${bank.sbif}</td>
+                    <td>${bank.name}</td> 
+                    <td>${bank.swift}</td> 
+                    <td><a href="${bank.website}" target="_blank">${bank.website}</a></td>
+                </tr>
+            `;
+      }).join("");
+
+    pintar += "</tbody></table>";  
+        contenedor.innerHTML = pintar;  
     } catch (error) {
-      console.error("Error:", error);
+        console.error("Error:", error);
     }
-  }
-  
-  async function usuario(id) {
-    try {
-      let respuesta = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${id}`
-      );
-      if (!respuesta.ok) {
-        throw new Error(`Error: ${respuesta.status}`);
-      }
-      let usuario = await respuesta.json();
-      console.log(usuario);
-    } catch (error) {
-      console.error("Error al ejecutar la consulta", error);
-    }
+
   }
